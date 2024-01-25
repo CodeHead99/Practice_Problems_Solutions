0407ae88b05e751cefce08758c238dc09dd1f015
AKSHITA SHARMA <100984809+CodeHead99@users.noreply.github.com>
2024-01-25 22:36:26 +0530

    Create 1143. Longest Common Subsequence.cpp

---
+++ Leetcode/1143. Longest Common Subsequence.cpp
@@ -0,0 +1,59 @@
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+
+ 
+
+Example 1:
+
+Input: text1 = "abcde", text2 = "ace" 
+Output: 3  
+Explanation: The longest common subsequence is "ace" and its length is 3.
+Example 2:
+
+Input: text1 = "abc", text2 = "abc"
+Output: 3
+Explanation: The longest common subsequence is "abc" and its length is 3.
+Example 3:
+
+Input: text1 = "abc", text2 = "def"
+Output: 0
+Explanation: There is no such common subsequence, so the result is 0.
+ 
+
+Constraints:
+
+1 <= text1.length, text2.length <= 1000
+text1 and text2 consist of only lowercase English characters.
+
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        if (text1.length() < text2.length()) {
+            return LCS(text1, text2);
+        }
+        return LCS(text2, text1);
+    }
+
+    int LCS(string s1, string s2) {
+        vector<vector<int>> M(2, vector<int>(s1.length() + 1, 0));
+
+        for (int i = 1; i <= s2.length(); i++) {
+            M[i % 2][0] = 0;
+            for (int j = 1; j <= s1.length(); j++) {
+                if (s1[j - 1] == s2[i - 1]) {
+                    M[i % 2][j] = M[(i - 1) % 2][j - 1] + 1;
+                } else {
+                    M[i % 2][j] = max(M[(i - 1) % 2][j - 1],
+                                max(M[(i - 1) % 2][j], M[i % 2][j - 1]));
+                }
+            }
+        }
+        return M[s2.length() % 2][s1.length()];
+    }
+};
